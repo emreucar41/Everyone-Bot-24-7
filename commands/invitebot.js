17dacb3bea60812f396acd0ed07ba82c7dbbc27e
emreucar41 <42539421+emreucar41@users.noreply.github.com>
2019-04-29 00:42:03 +0300

    Update invitebot.js

--- commands/invitebot.js
+++ commands/invitebot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    client.generateInvite().then(link => {
+        var link = "https://discordapp.com/oauth2/authorize?client_id=566363419180269599&permissions=8&scope=bot"
         var embed = new Discord.RichEmbed()
             .setColor("#808080")
             .addField("İnvite Link", "[Click Here](" + link + ")", false);
