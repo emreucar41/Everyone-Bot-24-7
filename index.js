154acf2215b4171c50fc9edc6c65fe68c93c83a3
emreucar41 <42539421+emreucar41@users.noreply.github.com>
2019-05-11 00:59:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
 let green = botconfig.green;
-let xp = require("./xp.json");
 let cooldown = new Set();
 let cdseconds = 5;
 bot.commands = new Discord.Collection();
@@ -37,37 +36,7 @@ bot.on("message", async message => {
 
    if (message.content.toLowerCase === 'ping') {
     message.channel.send('Pong!');
-  }
-
-   let xpAdd = Math.floor(Math.random() * 7)+ 8;
-   
-
-   if(!xp[message.author.id]){
-     xp[message.author.id] = {
-       xp: 0,
-       level: 1 
-     };
-   }
-
-   
-   let curxp = xp[message.author.id].xp;
-   let curlvl = xp[message.author.id].level;
-   let nxtlvl = xp[message.author.id].level * 300;
-   xp[message.author.id].xp = curxp + xpAdd;
-   if(nxtlvl <= xp[message.author.id].xp){
-     xp[message.author.id].level = curlvl + 1;
-   let lvlup = new Discord.RichEmbed()
-   .setTitle("Level Up!")
-   .setColor(green)
-   .addField("New Level", curlvl + 1);
-
-   message.channel.send(lvlup).then(msg => {msg.delete(5000)});
-
-   }
-   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
-  });
- 
+  } 
 
    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
